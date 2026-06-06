78c7b8f1131310aca85f0751c2084cc9c2d6bc96
Ezra <ezraplt228@gmail.com>
2026-06-06 19:52:58 +0300

    Initial commit

---
+++ src/main/java/ezra4ka/sVFurniture/api/SVFurnitureAPI.java
@@ -0,0 +1,17 @@
+package ezra4ka.sVFurniture.api;
+
+import org.bukkit.NamespacedKey;
+import org.bukkit.block.Block;
+import org.bukkit.inventory.ItemStack;
+
+public interface SVFurnitureAPI {
+
+    boolean isFurniture(Block block);
+
+    boolean isCustomItem(ItemStack stack);
+
+    String getCustomId(ItemStack stack);
+
+    NamespacedKey getItemNamespace();
+//dfsdd
+}
